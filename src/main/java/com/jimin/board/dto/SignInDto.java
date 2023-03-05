994bac323accc33d4db3f1341167d336ed1898e5
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-05 20:29:20 +0900

    feat: SignInDto 생성

---
+++ src/main/java/com/jimin/board/dto/SignInDto.java
@@ -0,0 +1,17 @@
+package com.jimin.board.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignInDto {
+    @NotBlank
+    private String userEmail;
+    @NotBlank
+    private String userPassword;
+}
