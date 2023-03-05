09ca44d4481b840508665f2d4bf2795feee17a53
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-05 20:25:30 +0900

    refactor: BoardApplication SpringBootApplication 애노테이션 파라미터 추가

--- src/main/java/com/jimin/board/BoardApplication.java
+++ src/main/java/com/jimin/board/BoardApplication.java
@@ -2,11 +2,12 @@ package com.jimin.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class BoardApplication {
 
 	public static void main(String[] args) {
@@ -22,5 +23,4 @@ public class BoardApplication {
 			}
 		};
 	}
-
 }
