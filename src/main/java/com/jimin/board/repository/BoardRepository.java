b8fe77998139a4bb609f09ac0336cdf083ae6c73
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-05 20:26:27 +0900

    feat: BoardRepository 구현

--- src/main/java/com/jimin/board/repository/BoardRepository.java
+++ src/main/java/com/jimin/board/repository/BoardRepository.java
@@ -4,6 +4,15 @@ import com.jimin.board.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<BoardEntity, Integer> {
+
+    public List<BoardEntity> findTop3ByBoardWriteDateAfterOrderByBoardLikeCountDesc(Date boardWriteDate);
+
+    public List<BoardEntity> findByOrderByBoardWriteDateDesc();
+
+    public List<BoardEntity> findByBoardTitleContains(String boardTitle);
 }
