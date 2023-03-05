f7da504f2a40380049d9ce6c8a9560b051681e56
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-05 20:24:23 +0900

    feat: AuthController 회원가입, 로그인 구현

--- src/main/java/com/jimin/board/controller/AuthController.java
+++ src/main/java/com/jimin/board/controller/AuthController.java
@@ -1,16 +1,27 @@
 package com.jimin.board.controller;
 
 import com.jimin.board.dto.ResponseDto;
+import com.jimin.board.dto.SignInDto;
+import com.jimin.board.dto.SignInResponseDto;
 import com.jimin.board.dto.SignUpDto;
+import com.jimin.board.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    AuthService authService;
+
     @PostMapping("/signUp")
     public ResponseDto<?> signUp (@RequestBody SignUpDto requestBody) {
-        System.out.println(requestBody.toString());
-        return null;
+        ResponseDto<?> result = authService.sighUp(requestBody);
+        return result;
+    }
+    @PostMapping("/signIn")
+    public ResponseDto<SignInResponseDto> signIn(@RequestBody SignInDto request) {
+        return authService.signIn(request);
     }
 }
