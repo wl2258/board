93af436bfe8cf2e8ac1af6f7ce0a343e7e206534
wl2258 <77067383+wl2258@users.noreply.github.com>
2023-03-05 20:29:48 +0900

    refactor: SignInResponseDto 필드 추가

--- src/main/java/com/jimin/board/dto/SignInResponseDto.java
+++ src/main/java/com/jimin/board/dto/SignInResponseDto.java
@@ -1,5 +1,6 @@
 package com.jimin.board.dto;
 
+import com.jimin.board.entity.UserEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,5 @@ import lombok.NoArgsConstructor;
 public class SignInResponseDto {
     private String token;
     private int exprTime;
+    private UserEntity user;
 }
